9130475ef5df1040abc74488eba0f4c39f640dbf
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2024-01-20 19:33:54 -0500

    it now works

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const path = require('path');
 
-// Assuming you have a user array or database setup
 const users = [{ id: 1, username: 'user', password: 'password' }];
 
 passport.use(new LocalStrategy(
     (username, password, done) => {
+        console.log('LocalStrategy', username, password);
         const user = users.find(u => u.username === username && u.password === password);
         if (!user) {
             return done(null, false);
@@ -17,53 +17,48 @@ passport.use(new LocalStrategy(
     }
 ));
 
-passport.serializeUser((user, done) => done(null, user.id));
+passport.serializeUser((user, done) => {
+    console.log('serializeUser', user);
+    done(null, user.id);
+});
+
 passport.deserializeUser((id, done) => {
+    console.log('deserializeUser', id);
     const user = users.find(u => u.id === id);
     done(null, user);
 });
 
 const app = express();
+// app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
     secret: 'secret',
     resave: false,
-    saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
-    // cookie: { maxAge: null }  // Setting maxAge to null will delete the cookie when the browser is closed
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware to redirect unauthenticated requests to login
-function ensureAuthenticated(req, res, next) {
-    console.log(req.isAuthenticated());
-    console.log(req, res, next)
-    if (!req.isAuthenticated()) {
-        return res.redirect('/login');
-    }
+app.use((req, res, next) => {
+    console.log('Request URL:', req.url);
     next();
-}
-
-// Middleware to destroy session after serving a page
-function destroySession(req, res, next) {
-        console.log(req.isAuthenticated());
-    console.log(req, res, next)
-    res.on('finish', () => {
-        req.session.destroy();
-    });
-    next();
-}
-
-app.get('/', destroySession, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/login',destroySession, (req, res) => {
+app.get('/', (req, res) => {
+    console.log('Accessing /');
     if (req.isAuthenticated()) {
-        return res.redirect('/');
+        console.log('User is authenticated');
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    } else {
+        console.log('User is not authenticated');
+        res.redirect('/login');
     }
+});
+
+app.get('/login', (req, res) => {
+    console.log('Accessing /login');
     res.sendFile(path.join(__dirname, 'public/login.html'));
 });
 
@@ -71,5 +66,19 @@ app.post('/login', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login'
 }));
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).send('Unauthorized');
+}
+
+app.get('/style.css', isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/style.css'));
+});
+
+app.get('/script.js', isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/script.js'));
+});
 
 app.listen(3000, () => console.log('Server started on port 3000'));
